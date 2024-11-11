ed2c28265f37e47763fb2f53f65281d73c39e7c8
Nabeel-DPP <nabeelhussain5359@gmail.com>
2024-11-11 16:52:00 +0500

    Last Changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -28,6 +28,24 @@ export default defineConfig({
 
 
 
+// import { defineConfig } from 'vite';
+// import ViteSass from 'vite-plugin-sass';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react(), ViteSass()],
+//   server: {
+//     port: 3000,
+//   },
+//   build: {
+//     minify: 'terser',          // For JS minification
+//     cssCodeSplit: false,       // Keep all CSS in one file to maintain order
+//   },
+//   css: {
+//     postcss: './postcss.config.cjs',  // Ensure Vite uses the PostCSS configuration
+//   },
+// });
+
 
 
 
